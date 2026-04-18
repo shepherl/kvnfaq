4de83892af4b8aceb34a881e03343e2326b01b30
Rushan Fayzullin <103266620+shepherl@users.noreply.github.com>
2026-04-18 22:08:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
-# 1. Уведомление в углу экрана
+# 1. Уведомление
 osascript -e 'display notification "Начинаю проверку связи..." with title "KVN Тест" sound name "Glass"'
 
 echo "--- Запуск пинга ---"
 
-# 2. Пингуем Google 3 раза
+# 2. Начало блока IF
 if ping -c 3 google.com; then
     echo "--- Успех! ---"
     
-    # 3. Диалоговое окно с вопросом
-    # Мы сохраняем ответ пользователя в переменную
+    # 3. Диалог (тут всё в одной строке, проверь кавычки)
     ANSWER=$(osascript -e 'display dialog "Связь есть! Открыть Google в браузере?" buttons {"Нет", "Да"} default button "Да" with title "Тест завершен"' -e 'button returned of result')
 
     if [ "$ANSWER" = "Да" ]; then
         echo "🌐 Открываю Google..."
         open "https://www.google.com"
-    else
-        echo "👌 Ок, браузер не трогаем."
-    fi
+    fi # Закрыли внутренний IF
 
 else
     echo "--- Ошибка ---"
     osascript -e 'display dialog "Нет связи. Проверь интернет! ❌" buttons {"Понял"} default button "Понял" with icon stop'
-fi.sh
+fi # Закрыли внешний IF (СКОРЕЕ ВСЕГО, ТУТ БЫЛА ОШИБКА)
