66c2e8a93f3458fb334725e841698bdd24507b43
Rushan Fayzullin <103266620+shepherl@users.noreply.github.com>
2026-04-18 22:14:08 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,28 +1,6 @@
-#!/bin/bash
+# ID FoxyProxy: gomekmidlodpcbbmibiiaebmhpbhhlca
+EXTENSION_ID="gomekmidlodpcbbmibiiaebmhpbhhlca"
+CRX_URL="https://clients2.google.com/service/update2/crx?response=redirect&os=mac&arch=arm64&os_arch=arm64&nacl_arch=arm64&prod=chromecrx&prodchannel=&prodversion=120.0.6099.129&lang=en-US&acceptformat=crx3&x=id%3D${EXTENSION_ID}%26installsource%3Dondemand%26uc"
 
-# 1. Уведомление в углу экрана
-osascript -e 'display notification "Начинаю проверку связи..." with title "KVN Тест" sound name "Glass"'
-
-echo "--- Запуск пинга ---"
-
-# 2. Пингуем Google 3 раза
-if ping -c 3 google.com; then
-    echo "--- Успех! ---"
-    
-    # 3. Диалоговое окно
-    ANSWER=$(osascript -e 'display dialog "Связь есть! Открыть Google в Chrome?" buttons {"Нет", "Да"} default button "Да" with title "Тест завершен"' -e 'button returned of result')
-
-    if [ "$ANSWER" = "Да" ]; then
-        echo "🌐 Пытаюсь открыть Google в Chrome..."
-        
-        # Пытаемся открыть в Chrome. Если его нет (||), открываем в обычном браузере.
-        open -a "Google Chrome" "https://www.google.com" || open "https://www.google.com"
-    else
-        echo "👌 Ок, браузер не трогаем."
-    fi
-
-else
-    echo "--- Ошибка ---"
-    # Окно ошибки, если пинг не прошел
-    osascript -e 'display dialog "Нет связи. Проверь интернет! ❌" buttons {"Понял"} default button "Понял" with icon stop'
-fi
+echo "📦 Вызываем окно установки FoxyProxy..."
+open -a "Google Chrome" "$CRX_URL"
