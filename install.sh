96f578320eeb06d2cba92b92cfb38d28cc8d10db
Rushan Fayzullin <103266620+shepherl@users.noreply.github.com>
2026-06-13 01:30:46 +0300

    Добавил проверку достаточно ли памяти

--- install.sh
+++ install.sh
@@ -3,39 +3,48 @@
 # Константы
 readonly APP_URL="https://github.com/shepherl/kvnfaq/releases/download/1.1/macOS-Intel-App.zip"
 readonly TMP_ZIP="/tmp/KVN_App.zip"
+# 300 МБ * 1024 = 307200 КБ
+readonly REQUIRED_SPACE_KB=307200
 
+# 1. Проверка свободного места
+free_space_kb=$(df -Pk ~ | awk 'NR==2 {print $4}')
+
+if [ "$free_space_kb" -lt "$REQUIRED_SPACE_KB" ]; then
+    osascript -e 'display dialog "Недостаточно места на диске для установки (нужно минимум 300 МБ)." with title "Ошибка" buttons {"ОК"} default button "ОК" with icon caution'
+    exit 1
+fi
+
+# 2. Проверка домена и установка
 if [[ $(hostname) == *.kzn.21-school.ru ]]; then
-# Скачивание и распаковка ZIP
-curl -L "$APP_URL" -o "$TMP_ZIP"
+    
+    # Скачивание
+    curl -L "$APP_URL" -o "$TMP_ZIP"
 
-# Распаковываем ZIP во временную папку, чтобы найти там DMG
-unzip -qo "$TMP_ZIP" -d /tmp/ 
+    # Распаковка
+    unzip -qo "$TMP_ZIP" -d /tmp/ 
 
-# Ищем DMG файл (даже если имя чуть отличается)
-DMG_PATH=$(ls /tmp/*.dmg | head -n 1) 
+    # Поиск DMG
+    DMG_PATH=$(ls /tmp/*.dmg | head -n 1) 
 
-# Монтируем DMG и получаем путь, куда он примонтировался
-MOUNT_POINT=$(hdiutil attach -nobrowse -noautoopen "$DMG_PATH" | grep -o '/Volumes/.*' | head -n 1) 
+    # Монтирование
+    MOUNT_POINT=$(hdiutil attach -nobrowse -noautoopen "$DMG_PATH" | grep -o '/Volumes/.*' | head -n 1) 
 
-# Копируем .app из DMG на Desktop
-cp -R "$MOUNT_POINT"/*.app ~/Desktop/
+    # Копирование .app
+    cp -R "$MOUNT_POINT"/*.app ~/Desktop/
 
-#Размонтирование DMG
-hdiutil detach "$MOUNT_POINT" -quiet
+    # Размонтирование и очистка
+    hdiutil detach "$MOUNT_POINT" -quiet
+    rm -f "$TMP_ZIP" "$DMG_PATH"
 
-# Очистка временных файлов
-rm "$TMP_ZIP" "$DMG_PATH"
+    # Снятие карантина и обновление иконки
+    xattr -cr ~/Desktop/KVN.app
+    touch ~/Desktop/KVN.app
 
-# Вывод KVN с карантина 
-xattr -cr ~/Desktop/KVN.app
-
-# Перечитываем иконку приложения
-touch ~/Desktop/KVN.app
-
-#Вызов окна с результатом
-osascript -e 'display dialog "Установка KVN успешно завершена! KVN на рабочем столе" with title "Установщик" buttons {"ОК"} default button "ОК" with icon note'
+    # Уведомление об успехе
+    osascript -e 'display dialog "Установка KVN успешно завершена! KVN на рабочем столе" with title "Установщик" buttons {"ОК"} default button "ОК" with icon note'
 else
-osascript -e 'display dialog "Ваше устройство не подходит" with title "Ошибка" buttons {"ОК"} default button "ОК" with icon note'
+    # Уведомление об ошибке
+    osascript -e 'display dialog "Ваше устройство не подходит" with title "Ошибка" buttons {"ОК"} default button "ОК" with icon note'
 fi
 
 
